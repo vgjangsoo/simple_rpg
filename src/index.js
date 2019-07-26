70e9e7e7145f56d073f8fc53fd0ec6bb7e2993f4
Sungil Saul Lee <jangsoo86@gmail.com>
2019-07-26 13:45:10 -0700

    Initial commit

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import logoImg from "./assets/logo.png";
 const config = {
   type: Phaser.AUTO,
   parent: "phaser-example",
-  width: 800,
-  height: 600,
+  width: 1100,
+  height: 800,
   scene: {
     preload: preload,
     create: create
@@ -19,7 +19,7 @@ function preload() {
 }
 
 function create() {
-  const logo = this.add.image(400, 150, "logo");
+  const logo = this.add.image(550, 150, "logo");
 
   this.tweens.add({
     targets: logo,
